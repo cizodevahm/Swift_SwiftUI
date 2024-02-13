3321bfb3f36e797e86cb1564e6ee66dc25752132
Cizotech Software Pvt Ltd <93611338+cizodevahm@users.noreply.github.com>
2024-02-13 12:20:05 +0530

    Add files via upload

---
+++ Swift_SwiftUI/SwiftUIView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  Swift_SwiftUI
+//
+//  Created by jignesh solanki on 09/02/24.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text("Hello, Users")
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
---
+++ Swift_SwiftUI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swift_SwiftUI
+//
+//  Created by jignesh solanki on 09/02/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
